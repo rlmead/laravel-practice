132672c3310000e6fbef506f2b16df8764f04036
Rebecca Mead <beckyleemead@gmail.com>
2020-11-09 13:18:21 -0500

    eloquent prettification

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,15 +9,8 @@ class PostController extends Controller
 {
     public function show($slug)
     {
-        // $post = DB::table('posts')->where('slug', $slug)->first();
-        $post = Post::where('slug', $slug)->first();
-        
-        if (! $post) {
-            abort(404);
-        }
-
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
