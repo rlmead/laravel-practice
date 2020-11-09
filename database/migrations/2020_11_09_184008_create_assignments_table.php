0b28af4d04d600f568b068b135f3a9b0ec6637ce
Rebecca Mead <beckyleemead@gmail.com>
2020-11-09 14:12:00 -0500

    finishing database access section

--- database/migrations/2020_11_09_184008_create_assignments_table.php
+++ database/migrations/2020_11_09_184008_create_assignments_table.php
@@ -15,7 +15,10 @@ class CreateAssignmentsTable extends Migration
     {
         Schema::create('assignments', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
+            $table->timestamp('due_date')->nullable();
         });
     }
 
