7a5fe6b26bdac8e5f0cdcb63da45f69db408e061
Rebecca Mead <beckyleemead@gmail.com>
2020-11-10 15:53:03 -0500

    30: foreign keys, factories

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -14,9 +14,9 @@ class Article extends Model
         return route('articles.show', $this);
     }
 
-    public function user()
+    public function author()
     {
-        //
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     // protected $guarded = [];
